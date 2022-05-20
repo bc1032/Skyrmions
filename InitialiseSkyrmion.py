26e1f69c9e8b6f5aa57aeb64f8735298b281a7c4
bc1032 <33351629+bc1032@users.noreply.github.com>
2022-05-20 16:16:24 +0100

    Add files via upload

--- InitialiseSkyrmion.py
+++ InitialiseSkyrmion.py
@@ -11,15 +11,14 @@ if not os.path.exists('results'):
 os.chdir('results')
 
 
-Lx, Ly = 51, 51
-R = 20
+Lx, Ly = 101, 101
+R = 40
 
 def initialise(Lx, Ly, R):
     theta = np.zeros([Lx, Ly])
     phi = np.zeros([Lx,Ly])
     centrex = int(Lx/2)
     centrey = int(Ly/2)
-
 
     for x in range(0,Lx):
         for y in range(0,Ly):
@@ -30,17 +29,17 @@ def initialise(Lx, Ly, R):
             if dx > R or dy > R:
                 phi[x,y] = math.pi/2
             elif dx + dy <= R:
-                phi[x,y] = math.atan2(y,x)
-                theta[x,y] =  math.pi*math.sqrt(x**2 + y**2)/(2*R)
+                phi[x,y] = math.pi/2 + math.atan2(y-centrey,x-centrex)
+                theta[x,y] =  math.pi*math.sqrt((x-centrex)**2 + (y-centrey)**2)/(2*R)
             elif math.sqrt(dx**2 + dy**2) < R:
-                phi[x,y] = math.atan2(y,x)
-                theta[x,y] =  math.pi*math.sqrt(x**2 + y**2)/(2*R)
+                phi[x,y] = math.pi/2 + math.atan2(y-centrey,x-centrex)
+                theta[x,y] =  math.pi*math.sqrt((x-centrex)**2 + (y-centrey)**2)/(2*R)
             else:
                 phi[x,y] = math.pi/2
 
     print(phi,theta)
     np.savetxt('phi.dat', phi)
     np.savetxt('theta.dat', theta)
-    return(0)
+    return(phi,theta)
 
 initialise(Lx,Ly,R)
