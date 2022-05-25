1ac950b8435c1ed53a5e6713a74026c81e26bc83
bc1032 <33351629+bc1032@users.noreply.github.com>
2022-05-25 10:01:39 +0100

    Add files via upload

--- SkyrmionEnergy.py
+++ SkyrmionEnergy.py
@@ -7,33 +7,41 @@ from scipy.optimize import minimize
 
 import InitialiseSkyrmion
 
-if not os.path.exists('results'):
-    os.makedirs('results')
-
-os.chdir('results')
+# if not os.path.exists('results'):
+#     os.makedirs('results')
+#
+# os.chdir('results')
 
 
 Lx, Ly = 101, 101
 R = 40
 
-def sphere(phi,theta,a,b,c):
+def sphere(a,b,c):
     dx = 1
     dy = 1
 
-    E = c*(-1 + math.cos(theta[x,y])**2) + b*(-(math.cos(phi[x,y])*(-theta[x,yb] + theta[x,ya]))/(2.*dy) + (math.sin(phi[x,y])*(-theta[xl,y] + theta[xr,y]))/(2.*dx) +\
-        math.cos(theta[x,y])*math.sin(theta[x,y])*((math.sin(phi[x,y])*(-phi[x,yb] + phi[x,ya]))/(2.*dy) + (math.cos(phi[x,y])*(-phi[xl,y] + phi[xr,y]))/(2.*dx))) + \
-        a*(math.cos(phi[x,y])*((math.cos(theta[x,y])*(-theta[xl,y] + theta[xr,y]))/(2.*dx) + (math.sin(theta[x,y])*(-phi[x,yb] + phi[x,ya]))/(2.*dy)) + \
-        math.sin(phi[x,y])*((math.cos(theta[x,y])*(-theta[x,yb] + theta[x,ya]))/(2.*dy) - (math.sin(theta[x,y])*(-phi[xl,y] + phi[xr,y]))/(2.*dx)))**2
+    phi = np.loadtxt('phi.dat')
+    theta = np.loadtxt('theta.dat')
+
+    for x in range(0,Lx):
+        for y in range(0,Ly):
+
+            E = c*(-1 + math.cos(theta[x,y])**2) + b*(-(math.cos(phi[x,y])*(-theta[x,yb] + theta[x,ya]))/(2.*dy) + (math.sin(phi[x,y])*(-theta[xl,y] + theta[xr,y]))/(2.*dx) +\
+                math.cos(theta[x,y])*math.sin(theta[x,y])*((math.sin(phi[x,y])*(-phi[x,yb] + phi[x,ya]))/(2.*dy) + (math.cos(phi[x,y])*(-phi[xl,y] + phi[xr,y]))/(2.*dx))) + \
+                a*(math.cos(phi[x,y])*((math.cos(theta[x,y])*(-theta[xl,y] + theta[xr,y]))/(2.*dx) + (math.sin(theta[x,y])*(-phi[x,yb] + phi[x,ya]))/(2.*dy)) + \
+                math.sin(phi[x,y])*((math.cos(theta[x,y])*(-theta[x,yb] + theta[x,ya]))/(2.*dy) - (math.sin(theta[x,y])*(-phi[xl,y] + phi[xr,y]))/(2.*dx)))**2
 
     return(E)
 
 def projection(u,v):
 
-    E = (c*(1 - u[x,y]**2 - v[x,y]**2)**2)/(1 + u[x,y]**2 + v[x,y]**2)**2 +
-        (b*(-(((-u[x,yb] + u[x,ya])*(1 + u[x,y]**2 - v[x,y]**2))/dy) + 4*u[x,y]*v[x,y]*((-u[xl,y] + u[xr,y])/(2.*dx) - (-v[x,yb] + v[x,ya])/(2.*dy)) +
-        ((1 - u[x,y]**2 + v[x,y]**2)*(-v[xl,y] + v[xr,y]))/dx))/(1 + u[x,y]**2 + v[x,y]**2)**2 +
-        (4*a*(((-u[xl,y] + u[xr,y])*(1 + v[x,y]**2))/(2.*dx) - ((-1 + v[x,y]**2)*(-v[x,yb] + v[x,ya]))/(2.*dy) +
-        u[x,y]**2*(-(-u[xl,y] + u[xr,y])/(2.*dx) + (-v[x,yb] + v[x,ya])/(2.*dy)) -
+    E = (c*(1 - u[x,y]**2 - v[x,y]**2)**2)/(1 + u[x,y]**2 + v[x,y]**2)**2 +\
+        (b*(-(((-u[x,yb] + u[x,ya])*(1 + u[x,y]**2 - v[x,y]**2))/dy) + 4*u[x,y]*v[x,y]*((-u[xl,y] + u[xr,y])/(2.*dx) - (-v[x,yb] + v[x,ya])/(2.*dy)) +\
+        ((1 - u[x,y]**2 + v[x,y]**2)*(-v[xl,y] + v[xr,y]))/dx))/(1 + u[x,y]**2 + v[x,y]**2)**2 +\
+        (4*a*(((-u[xl,y] + u[xr,y])*(1 + v[x,y]**2))/(2.*dx) - ((-1 + v[x,y]**2)*(-v[x,yb] + v[x,ya]))/(2.*dy) +\
+        u[x,y]**2*(-(-u[xl,y] + u[xr,y])/(2.*dx) + (-v[x,yb] + v[x,ya])/(2.*dy)) -\
         2*u[x,y]*v[x,y]*((-u[x,yb] + u[x,ya])/(2.*dy) + (-v[xl,y] + v[xr,y])/(2.*dx)))**2)/(1 + u[x,y]**2 + v[x,y]**2)**4
 
     return(E)
+
+sphere(1,2,3)
